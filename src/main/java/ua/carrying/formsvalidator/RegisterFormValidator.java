dbb804d2716a94c21022abd5a0931e889cebcd23
roolrd <ruslanriznyk@gmail.com>
2019-01-05 18:48:16 +0200

    Забрав зайве з форми валідації

--- src/main/java/ua/carrying/formsvalidator/RegisterFormValidator.java
+++ src/main/java/ua/carrying/formsvalidator/RegisterFormValidator.java
@@ -56,20 +56,7 @@ public class RegisterFormValidator {
             return false;
         }
 
-        /* check userName full name (first and second)
-        CheckField checkFielduserName = field -> {
-            boolean res = field.matches("^[A-Za-zА-Яа-яЄєЇїІіЪъЫыЭэ\\s]{1,}[\\.]{0,1}[A-Za-zА-Яа-яЄєЇїІіЪъЫыЭэ\\s]{0,}$");
-            if (!res) {
-                messageError = "Invalid userName!";
-
-                            }
-            return res;
-        };
-        if (!checkFielduserName.isCorrect(userName)) {
-            return false;
-        } */
-
-        //check email if correct
+         //check email if correct
         CheckField checkFieldEmail = field -> {
             boolean res =  field.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
                     + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
@@ -88,7 +75,8 @@ public class RegisterFormValidator {
         CheckField checkFieldPassword = field -> {
             boolean res = field.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,}$");
             if (! res) {
-                messageError = "Your password has low safety. It will has must: a min. 6 symbols, at least once a digit, a lower case letter, an upper case letter, no whitespace allowed in the entire string!";
+                messageError = "Your password has low safety. It will has must: a min. 6 symbols, at least once a digit," +
+                        "a lower case letter, an upper case letter, no whitespace allowed in the entire string, only latin letters!";
             }
             return res;
         };
@@ -96,21 +84,6 @@ public class RegisterFormValidator {
             return false;
         }
 
-        /*
-        //check phone if correct
-        CheckField checkFieldPhone = field -> {
-            boolean res = field.matches("^/(?<!\\w)(?:(?:(?:(?:\\+?3)?8\\W{0,5})?0\\W{0,5})?[34569]\\s?\\d[^\\w,;(\\+]{0,5})?\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d\\W{0,5}\\d(?!(\\W?\\d))/x$");
-            if (!res) {
-                messageError = "Invalid phone number!";
-            }
-            return res;
-        };
-
-        if (!checkFieldPhone.isCorrect(phone)) {
-            return false;
-        }
-        */
-
-        return true;
+         return true;
     }
 }
